1ace21d1deb13f87dfab6464025ec4f6bb33b885
nmcpallas <nmcpallas@gmail.com>
2023-08-13 17:19:50 +0700

    [chapter-1] app was created

---
+++ src/test/java/taco/cloud/TacoCloudApplicationTests.java
@@ -0,0 +1,12 @@
+package taco.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TacoCloudApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+}
